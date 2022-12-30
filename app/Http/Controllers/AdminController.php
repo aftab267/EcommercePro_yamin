7527dcf651a4e93aa78a6c61edd2a4c5769937ab
kazi-aftabur-rahman <aftab267@gmail.com>
2022-12-30 22:54:56 +0600

    category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class AdminController extends Controller
 {
     public function view_category(){
-        return view('admin.category');
+        $data=Category::all();
+        return view('admin.category',compact('data'));
+    }
+    public function add_category(Request $request){
+        $data=new Category;
+        $data->category_name=$request->category;
+        $data->save();
+        return redirect()->back()->with('message','Category Added Successfully');
+
+    }
+    public function delete_category($id){
+        $data=Category::find($id);
+        $data->delete();
+        return redirect()->back()->with('message','Data deleted Successfully.');
     }
 }
--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -12,6 +12,16 @@
                             font-size: 40px;
                             padding-bottom: 40px;
                         }
+                        .input_color{
+                            color: black;
+                        }
+                        .center{
+                            margin: auto;
+                            text-align: center;
+                            width: 50%;
+                            margin-top: 30px;
+                            border: 1px solid white;
+                        }
                     </style>
 
                     @include('admin.sidebar')
@@ -20,13 +30,41 @@
                         <!-- partial -->
                         <div class="main-panel">
                             <div class="content-wrapper">
+                                @if(session()->has('message'))
+                                    <div class="alert alert-success">
+                                        <button type="button" class="close"  data-dismiss="alert" aria-hidden="true"></button>
+                                        {{session()->get('message')}}
+                                    </div>
+                                 @endif
+                                    <div class="div_center ">
+                                        <h2 class="h2_font">Add Category</h2>
+                                        <form action="{{ ('/add_category') }}" method="POST">
+                                            @csrf
 
-                                <div class="div_center">
-                                    <h2 class="h2_font">Add Category</h2>
-                                </div>
+                                            <input type="text" class="input_color" name="category" placeholder="Write Category Name"><br><br>
+                                            <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                                        </form>
 
-                            </div>
-                            </div>
+                                    </div>
+                                  
+                                    <table class="center">
+                                        <tr>
+                                            <td>Category Name</td>
+                                            <td>Action</td>
+                                        </tr>
+                                        @foreach($data as $da)
+                                        <tr>
+                                            <td>{{ $da->category_name }}</td>
+                                            <td>
+                                                <a class="btn btn-success" href="">Edit</a>
+                                                <a onclick="return confirm('Are You Sure you want to delete?')" class="btn btn-danger" href="{{ url('delete_category',$da->id) }}">Delete</a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                      </table>
+
+                              </div>
+                        </div>
         <!-- main-panel ends -->
       </div>
       <!-- page-body-wrapper ends -->
